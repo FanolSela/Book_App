d1ace4c411386e898dee7a30881a3b86549d486b
Fanol Sela <fanol.sela@yahoo.com>
2021-01-12 13:02:54 -0500

    itsover?

--- book-app/src/services/apiConfig.js
+++ book-app/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-      ? 'https://sei-products-api.herokuapp.com'
+      ? 'https://manga-guy.herokuapp.com'
       : 'http://localhost:3000'
 })
 
